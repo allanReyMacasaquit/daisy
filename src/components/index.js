03e22b2ef4250154a0b8fea4f2ef15656d1619b6
allan <allanrey.macasaquit@gmail.com>
2024-04-06 19:04:33 -0400

    Navbar with navlinks component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as FormInput } from './form/FormInput';
 export { default as SubmitBtn } from './form/SubmitBtn';
 export { default as Header } from './header/Header';
+export { default as Navbar } from './navbar/Navbar';
