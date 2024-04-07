7f47ecce31c0c52db5047b8879a622763adca900
allan <allanrey.macasaquit@gmail.com>
2024-04-06 21:22:35 -0400

    Hero

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as FormInput } from './form/FormInput';
 export { default as SubmitBtn } from './form/SubmitBtn';
 export { default as Header } from './header/Header';
 export { default as Navbar } from './navbar/Navbar';
+export { default as Hero } from './hero/Hero';
--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -16,7 +16,7 @@ const NavLinks = () => {
 				const { id, url, text } = link;
 				return (
 					<li key={id}>
-						<NavLink className='capitalize' to={url}>
+						<NavLink className='capitalize mx-2' to={url}>
 							{text}
 						</NavLink>
 					</li>
